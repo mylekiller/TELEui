6f70622e18dad876498a985a5a8bd25c3980db9c
Kyle Miller <kmille42@nd.edu>
2019-04-20 01:04:02 -0400

    Added Angular Material Code and added come material components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatButtonModule, MatCheckboxModule, MatToolbarModule, MatFormFieldModule, MatDividerModule} from '@angular/material';
+
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +11,13 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatToolbarModule,
+    MatFormFieldModule,
+    MatDividerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
