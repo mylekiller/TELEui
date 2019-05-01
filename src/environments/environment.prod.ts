5b52f0ca23b6a98f8d2d24841ea012418d4a6659
Kyle Miller <kmille42@nd.edu>
2019-05-01 01:22:59 -0400

    add api keys

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyADINa2G7hN0Cq4mDGclOTVJRghOPCWYsU',
+    authDomain: 'teleui.firebaseapp.com',
+    databaseURL: 'https://teleui.firebaseio.com',
+    projectId: 'teleui',
+    storageBucket: 'teleui.appspot.com',
+    messagingSenderId: '58407694023'
+  }
 };
